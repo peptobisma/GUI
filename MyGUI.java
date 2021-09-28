90e0525ed7451abc15c049989fa78201445ba508
Bisma Suleman <34171072+peptobisma@users.noreply.github.com>
2021-09-28 17:47:29 +0200

    init

--- MyGUI.java
+++ MyGUI.java
@@ -1,18 +1,25 @@
+import java.awt.*;
 import javax.swing.*;
 
 public class MyGUI {
-    JFrame frame = new JFrame();
 
-    JPanel panel = new JPanel();
-    panel.setBorder(BorderFacctory.createEmptyBorder(30, 30, 10, 30));
-    panel.setLayout(new GridLayout());
+    public GUI() {
 
+        JFrame frame = new JFrame();
 
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0,1));
 
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Our GUI");
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+    
     public static void main(String[] args) {
         new GUI();
     }
-
-
-    
 }
